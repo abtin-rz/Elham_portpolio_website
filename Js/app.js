ecf4e426b20812462e00e29759a2c9719ae86427
abtin-rz <abtinrezaie@gmail.com>
2026-05-19 14:25:28 +0330

    +add gray curtain behind the hamburger menu

--- Js/app.js
+++ Js/app.js
@@ -1,6 +1,13 @@
 const toggle_btn = document.getElementById("toggle-btn");
 const toggle_menu = document.getElementById("toggle-menu");
+const curtain = document.getElementById("curtain");
 
-toggle_btn.addEventListener("click", function() {
+toggle_btn.addEventListener("click", function () {
     toggle_menu.classList.toggle("show-menu")
+    curtain.classList.toggle("show-curtain")
+})
+
+curtain.addEventListener("click", function () {
+    toggle_menu.classList.toggle("show-menu")
+    curtain.classList.toggle("show-curtain")
 })
